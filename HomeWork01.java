ae7e4378c8649404cbf33d34fa432fa8618ba29b
pan882244 <panzhifeng007@gmail.com>
2017-03-15 08:38:51 +0800

    Add files via upload

---
+++ HomeWork01.java
@@ -0,0 +1,25 @@
+public class HomeWork01 {
+	public static void main(String[] args) {
+		/*
+		定义变量的语法：
+			数据类型 变量名 ;
+			
+			数据类型 变量名1,变量名2 ;
+			
+			数据类型 变量名 = 值;
+			
+			数据类型 变量名1 = 值,变量名2 = 值　;
+		
+		*/
+		int num1 = 100 ;
+		float num2 = 3.14F ;
+		double num3 = 12.8 ;
+		
+		double num4 = num1 + num2 + num3 ;
+		
+		//System.out.println("和是："+num4) ;
+		System.out.printf("和是:%.2f",num4) ;
+		
+		
+	}
+}
